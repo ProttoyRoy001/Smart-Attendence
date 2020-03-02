345c87dfcf52ed8d8e362530a4bc0dc0f3b6d088
prottoy <prottoyroy01@gmail.com>
2020-03-02 23:07:54 +0600

    Start(java)

--- app/src/main/java/com/example/smartattendence/Start.java
+++ app/src/main/java/com/example/smartattendence/Start.java
@@ -2,13 +2,28 @@ package com.example.smartattendence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Start extends AppCompatActivity {
+public class Start<Button> extends AppCompatActivity {
+
+    private View button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        button = (View) findViewById(R.id.startId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Start.this,Main2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
