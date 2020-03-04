70ce4c71ed86bff8c084b479806923be9342d902
prottoy <prottoyroy01@gmail.com>
2020-03-04 23:12:31 +0600

    Creatting SQLite Database 1

--- app/src/main/java/com/example/smartattendence/QRcode.java
+++ app/src/main/java/com/example/smartattendence/QRcode.java
@@ -1,14 +1,46 @@
 package com.example.smartattendence;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import Databases.DbHelper;
+import Model.ListItem;
 
 public class QRcode extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<ListItem> arrayList;
+    DbHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_rcode);
+
+        recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        helper = new DbHelper(this);
+
+        //fetch the data from database.... if it is available, show it using recyclerview adpter
+
+        arrayList = helper.getAllInformation();
+
+        if(arrayList.size()>0)
+        {
+            //data is available set is to adpter
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"No data found",Toast.LENGTH_LONG).show();
+
+        }
+
+
     }
 }
