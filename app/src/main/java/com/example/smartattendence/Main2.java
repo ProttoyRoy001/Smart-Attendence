2a0eb62cb5ac59316883815e052f4f81ddf73edd
prottoy <prottoyroy01@gmail.com>
2020-03-04 21:44:45 +0600

    build.gradle compile, QR(xml)

--- app/src/main/java/com/example/smartattendence/Main2.java
+++ app/src/main/java/com/example/smartattendence/Main2.java
@@ -2,13 +2,27 @@ package com.example.smartattendence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2 extends AppCompatActivity {
+
+    private View button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button1 = (View) findViewById(R.id.addstuddentId);
+         button1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(Main2.this,QRcode.class);
+                 startActivity(intent);
+             }
+         });
+
     }
 }
